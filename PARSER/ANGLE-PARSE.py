b959f1712a190c797788403422e052d001c21b8e
Harris <hmoham25@illinois.edu>
2020-01-04 23:43:56 -0600

    Adding the initial attempts to combine the angle and LIDAR data

--- PARSER/ANGLE-PARSE.py
+++ PARSER/ANGLE-PARSE.py
@@ -7,11 +7,41 @@ import re
 import numpy as np
 import PARSER as lidar_parser  
 
-lidar_4 = '../LOGS/attempt-4-lidar.log'
-angle_4 = '../LOGS/attempt-4.log'
-lidar_5 = '../LOGS/attempt-5-lidar.log'
-angle_5 = '../LOGS/attempt-5.log'
-lidar_6 = '../LOGS/attempt-6-lidar.log'
-angle_6 = '../LOGS/attempt-6.log'
+lidar_4 = '../../sample_logs/attempt-4-lidar.log'
+angle_4 = '../../sample_logs/attempt-4.log'
+lidar_5 = '../../sample_logs/attempt-5-lidar.log'
+angle_5 = '../../sample_logs/attempt-5.log'
+lidar_6 = '../../sample_logs/attempt-6-lidar.log'
+angle_6 = '../../sample_logs/attempt-6.log'
 
 lidar_p4 = lidar_parser.parser(lidar_4)
+lidar_p5 = lidar_parser.parser(lidar_5)
+lidar_p6 = lidar_parser.parser(lidar_6)
+
+# Function: Angle parser 
+# Description: Goes through the PUTTY log files and returns a dict
+def angle_parser(file):
+    init_parse = lidar_parser.load_file(file)
+    init_parse = init_parse[1:]
+
+    for index in range(len(init_parse)):
+        curr = init_parse[index]
+        init_parse[index] = curr.strip('\n') 
+    
+    return init_parse
+
+# Function: Angle/LIDAR merger
+# Description: Appropriately truncates and then merges angle data and a LIDAR scan
+def merge(angles, lidar):
+    print("Angles before: ", len(angles))
+    print("LIDAR before: ", len(lidar))
+    if len(angles) > len(lidar):
+        angles = angles[:len(angles) - len(lidar)]
+    elif len(lidar) > len(angles):
+        lidar = lidar[:len(lidar) - len(angles)]
+    print("Angles after: ", len(angles))
+    print("LIDAR after: ", len(lidar))
+    
+
+# test_angle = angle_parser(angle_4)
+test_merge = merge(angle_parser(angle_4), lidar_4)
