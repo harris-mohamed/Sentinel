016438c7f15e3871a4a9917b4ae9534cd02d93a8
nicko627 <58644280+nicko627@users.noreply.github.com>
2020-01-12 22:04:07 -0600

    Parameters tuned
    The parameters have been tuned to decent values:
    
    Kp = 0.5;
    Ki = 5.0;
    Kd = 0.0;
    
    These should be a good first guess for placing them on the robot. The weight of the robot will likely reduce the reactivity of the system, so Kd might be more necessary.

--- COMM/MOTOR/motor_pwm_pid/motor_pwm_pid.ino
+++ COMM/MOTOR/motor_pwm_pid/motor_pwm_pid.ino
@@ -5,9 +5,10 @@
 #define B 3
 #define forwardPin 5 
 #define reversePin 6
+#define pot A15
 
 double Setpoint, actual_rpm, Output;
-long oldPosition  = -999;
+long oldPosition  = 0; //this was made zero to prevent the code from reacting to it at initial startup.
 double cycle_revs = 0;
 double previous_change;
 double deg;
@@ -18,9 +19,11 @@ double rev_count = 0;
 unsigned long starttime = 0;
 unsigned long stoptime = 1;
 int i = 1;
-const double Kp = 0.001; //tuning parameter for "error-proportional" term.
-const double Ki = 0.001; //tuning parameter for "error-integral" term.
-const double Kd = 0.001; //tuning parameter for "error-derivative" term. 
+const double Kp = 0.5; //tuning parameter for "error-proportional" term.
+const double Ki = 7.0; //tuning parameter for "error-integral" term.
+const double Kd = 0.; //tuning parameter for "error-derivative" term.For Now, it seems that this can stay 0. It introduces instability at larger values. 
+
+const double maxRPM = 130.0;
 
 
 PID myPID(&actual_rpm, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
@@ -28,14 +31,14 @@ Encoder myEnc(A, B);
 
 void setup() {
   Serial.begin(9600);
-  Setpoint = 10;
+  Setpoint = analogRead(pot)*maxRPM/1023.00;
   actual_rpm = 0;
   pinMode(A, INPUT);
   pinMode(B, INPUT);
   pinMode(forwardPin, OUTPUT);
   pinMode(reversePin, OUTPUT);
-  digitalWrite(forwardPin, HIGH);
-  digitalWrite(reversePin, HIGH);
+//  digitalWrite(forwardPin, HIGH);
+//  digitalWrite(reversePin, HIGH);
   myPID.SetMode(AUTOMATIC); //start the PID
   Serial.println("done initializing");
 }
@@ -51,27 +54,37 @@ void loop() {
     previous_change = (newPosition-oldPosition)/1080.0;
     deg = rev_count * rev; 
     oldPosition = newPosition;
-    if (deg > rev){
-      deg -= 360;
+    float abs_save = abs(rev_count);
+    abs_save = abs_save - floor(abs_save);
+    
+    if (rev_count > 0){
+      rev_count = abs_save;
+    } 
+    else if (rev_count < 0) {
+      rev_count = -abs_save;  
     }
   }
   cycle_revs += previous_change;
   delta_t = millis()-starttime;
-  if (delta_t>100) { //after every 0.1 revolutions, calculate the approximate rpm
+  if (delta_t>50) { //after every 100 milliseconds, calculate the approximate rpm
     actual_rpm = cycle_revs*60000.0/delta_t; //revolutions per second
     i=1;
     cycle_revs = 0;
     previous_change = 0;
+    Setpoint = analogRead(pot)*maxRPM/1023.00;
     myPID.Compute();
   }
-  Serial.print("Measured Speed (rev/min): ");
+  Serial.print(" Set Point: ");
+  Serial.print(Setpoint);
+  Serial.print(" (PWM: ");
+  Serial.print(Setpoint*255.0/maxRPM);
+  Serial.print(") Measured Speed (rev/min): ");
   Serial.print(actual_rpm);
-  Serial.print(" OUTPUT: ");
-  Serial.print(Output);
+  Serial.print(" Applied PWM: ");
+  Serial.println(Output);
 //  Serial.print(" revs: ");
 //  Serial.println(cycle_revs);
-  Serial.print(" Degrees: ");
-  Serial.println(deg);
+  
   
    analogWrite(reversePin, 0);
    analogWrite(forwardPin, Output);
