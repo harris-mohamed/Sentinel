ecf616e459351e2a659ab5368a72d1a1709bd84c
harris <hmoham25@illinois.edu>
2020-03-01 14:43:47 -0600

    Adding the sentinel class

--- RPI/PARSER/SENTINEL.py
+++ RPI/PARSER/SENTINEL.py
@@ -90,25 +90,214 @@ class SENTINEL:
         s.uploadToAws(current_scan)
         return current_scan
 
-    def custom_message(self):
-        """Sends a custom message to the LIDAR sensor
+    def type_conv(self, num, base):
+        """Convert a string to an integer with the proper representation
 
-        Args:
-            None
-        Return:
-            None
+            Args:
+                num (string): String representation of number we wish to convert
+                base (string): Specifies the base we would like to convert to. Format is 'xy', 
+                            where x designates sign (u for unsigned, s for signed) and y is 
+                            number of bits the number is.
+            Return:
+                The converted number.
         """
+        initial = int(num, 16)
+        if base == 's8':
+            conv = (initial + 2**7) % 2**8 - 2**7
+        elif base == 'u8':
+            conv = initial % 2**8 
+        elif base == 's16':
+            conv = (initial + 2**15) % 2**16 - 2**15
+        elif base == 'u16':
+            conv = initial % 2**16 
+        elif base == 's32':
+            conv = (initial + 2**31) % 2**32 - 2**31
+        elif base == 'u32':
+            conv = initial % 2**32
+        elif base == 's64':
+            conv = (initial + 2**63) % 2**64 - 2**63
+        elif base == 'u64':
+            conv = initial % 2**64
+        else:
+            return initial
+        
+        return conv
 
-        start = '\x02'
-        end = '\x03'
-        message = input("Enter the custom message here:")
-        custom = start + message + end 
-        custom = str.encode(custom)
-        sock = connect()
-        sock.send(custom)
-        scan = message_collect(sock)
 
-        print("Output: ", scan)
+    def telegram_parse(self, scan):
+        """ Converts a raw scan into a neat dictionary  
+
+            Args:
+                scan (array): Raw scan data 
+            Return:
+                Dictionary of parsed values
+        """
+        telegram = {'Version Number': '',
+                    'Device Number': '',
+                    'Serial Number': '',
+                    'Device Status': '',
+                    'Telegram Counter': '',
+                    'Scan Counter': '',
+                    'Time since start-up': '',
+                    'Time of transmission': '',
+                    'Scan Frequency': '',
+                    'Measurement Frequency': '',
+                    'Amount of Encoder': '',
+                    '16-bit Channels': '',
+                    'Scale Factor': '',
+                    'Scale Factor Offset': '',
+                    'Start Angle': '',
+                    'Angular Increment': '', 
+                    'Quantity': '',
+                    'Motor encoder': '',
+                    'Timestamp': '',
+                    'Ax': '',
+                    'Ay': '',
+                    'Az': '',
+                    'Gx': '',
+                    'Gy': '',
+                    'Gz': '',
+                    'Measurement': [] }
+
+        if (scan[1] != 'LMDscandata'):
+            print("There is something wrong with the scan data")
+        else:
+            telegram['Version Number'] = type_conv(scan[2], 'u16')
+            telegram['Device Number'] = type_conv(scan[3], 'u16')
+            telegram['Serial Number'] = type_conv(scan[4], 'u32')
+            telegram['Device Status'] = type_conv(scan[6], 'u8')
+            telegram['Telegram Counter'] = type_conv(scan[7], 'u16')
+            telegram['Scan Counter'] = type_conv(scan[8], 'u16')
+            telegram['Time since start-up'] = type_conv(scan[9], 'u32') * microsecond
+            telegram['Time of transmission'] = type_conv(scan[10], 'u32') * microsecond
+            telegram['Scan Frequency'] = type_conv(scan[16], 'u32') 
+            telegram['Measurement Frequency'] = type_conv(scan[17], 'u32') 
+            telegram['Amount of Encoder'] = type_conv(scan[18], 'u32')
+            telegram['16-bit Channels'] = type_conv(scan[19], 'u32')
+            telegram['Scale Factor'] = scale_factor[scan[21]]
+            telegram['Scale Factor Offset'] = type_conv(scan[22], 'u32')
+            telegram['Start Angle'] = type_conv(scan[23], 's32') / angle_step
+            telegram['Angular Increment'] = type_conv(scan[24], 'u16') / angle_step
+            telegram['Quantity'] = type_conv(scan[25], 'u16')
+
+            for message in range(26, 26 + telegram['Quantity']):
+                telegram['Measurement'].append(type_conv(scan[message], 'u16'))
+            
+            telegram['Timestamp'] = scan[-1]
+            
+            Ax, Ay, Az, Gx, Gy, Gz = accel_read()
+            telegram['Ax'] = Ax
+            telegram['Ay'] = Ay
+            telegram['Az'] = Az
+            telegram['Gx'] = Gx
+            telegram['Gy'] = Gy
+            telegram['Gz'] = Gz
+
+        return telegram 
+
+    def live_parse(self, count):
+        """ Scans as many consecutive scans as necessary  
+
+            Args:
+                scan (array): Raw scan data 
+            Return:
+                Dictionary of parsed values
+        """
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((IP_ADDRESS, PORT))
+        sock.send(REQUEST_CONT_SCAN)
+
+        scans = []
+        while 1:
+            scan = []
+            curr = ''
+            while 1:
+                msg_orig = sock.recv(1)
+                msg = msg_orig.decode('utf-8')
+
+                if msg_orig == b'\x03': 
+                    scan.append(curr)
+                    break
+                elif msg == ' ':
+                    scan.append(curr)
+                    curr = ''
+                else:
+                    curr += msg
+            
+            if (len(scan) < 4):
+                continue
+
+            curr = datetime.now()
+            nice_timestamp = str(curr.year) + "-" + str(curr.month) + "-" + str(curr.day) + "_" + str(curr.hour) + "-" + str(curr.minute) + "-" + str(curr.second)
+
+            scan.append(nice_timestamp)
+
+            initial_parse = telegram_parse(scan)
+            scans.append(initial_parse)
+            
+            if len(scans) == count:
+                sock.send(STOP_CONT_SCAN)
+                break
+
+    def single_parse(self):
+        """ Parses a single scan from the sensor
+
+            Args:
+                None 
+            Return:
+                Dictionary of parsed values
+        """
+        insideTelegram = False 
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((IP_ADDRESS, PORT))
+        sock.send(REQUEST_SINGLE_SCAN)
+
+        scan = []
+        curr = ''
+        while 1:
+            msg_orig = sock.recv(1)
+            msg = msg_orig.decode('utf-8')
+            if msg_orig == b'\x02':
+                insideTelegram = True 
+            elif msg_orig == b'\x03':
+                insideTelegram = False 
+                break
+            elif msg == ' ':
+                scan.append(curr)
+                curr = ''
+            else:
+                curr += msg
+        
+        curr = datetime.now()
+        nice_timestamp = str(curr.year) + "-" + str(curr.month) + "-" + str(curr.day) + "_" + str(curr.hour) + "-" + str(curr.minute) + "-" + str(curr.second)
+
+        scan.append(nice_timestamp)
+
+        initial_parse = telegram_parse(scan)
+        read_serial = ser.readline()
+        return initial_parse
+
+    def uploadToAWS(self, telegram):
+
+        def custom_message(self):
+            """Sends a custom message to the LIDAR sensor
+
+            Args:
+                None
+            Return:
+                None
+            """
+
+            start = '\x02'
+            end = '\x03'
+            message = input("Enter the custom message here:")
+            custom = start + message + end 
+            custom = str.encode(custom)
+            sock = connect()
+            sock.send(custom)
+            scan = message_collect(sock)
+
+            print("Output: ", scan)
 
         
     def read_freq_angle(self):
