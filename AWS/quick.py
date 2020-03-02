bc9b78b3d2f8b9219fb8ce2b707c7864702039c8
harris <hmoham25@illinois.edu>
2020-03-02 12:46:08 -0600

    Adding some AWS testing files

--- AWS/quick.py
+++ AWS/quick.py
@@ -5,12 +5,21 @@ import decimal
 from boto3.dynamodb.conditions import Key, Attr
 
 dynamodb = boto3.resource('dynamodb', region_name='us-east-2', endpoint_url='http://dynamodb.us-east-2.amazonaws.com')
-table = dynamodb.Table('Sentinel')
+table = dynamodb.Table('SENTINEL')
 
-response = table.query(
-    KeyConditionExpression=Key('Time-stamp').eq('Yeet')
-)
+response = table.put_item(
+        Item={
+        'Name': 'Scan 1',
+        'Count': '0'
+    }
+    )
 
 
-for i in response['Items']:
-    print(type(i))
+
+# response = table.query(
+#     KeyConditionExpression=Key('Time-stamp').eq('Yeet')
+# )
+
+
+# for i in response['Items']:
+#     print(type(i))
