3c38cb6f7ea8999dd9482c62361ed9d58d2e647e
nicko627 <58644280+nicko627@users.noreply.github.com>
2020-04-12 19:01:25 -0500

    Edited RANSAC

--- RPI/SLAM/ransac_test.py
+++ RPI/SLAM/ransac_test.py
@@ -17,11 +17,11 @@ import quick
 import RANSAC.RANSAC as ransac
 import KALMAN as kalman
 
-X = 10 #mm, the maximum distance a point must be from an LSRP in RANSAC to be considered in tolerance.
-C = 1000 #Consensus for RANSAC, number of points that must pass the tolerance check of the LSRP
-N = 0 #Max number of trials in RANSAC before ending
+X = 0.005 #unitless, the maximum distance a point must be from an LSRP in RANSAC to be considered in tolerance.
+C = 500 #Consensus for RANSAC, number of points that must pass the tolerance check of the LSRP
+N = 30 #Max number of trials in RANSAC before ending
 S = 10 #Number of points to sample for RANSAC
-S_LIM = 100 #mm, half the length of a side of the cube to draw around the randomly sampled point in RANSAC
+S_LIM = 0.05 #unitless, half the length of a side of the cube to draw around the randomly sampled point in RANSAC
 
 LSRP_list = []
 ##
@@ -34,30 +34,43 @@ for scan in scan_kalman:
     scan['euler'] = kalman.Gravity([[float(scan['Ax'])], [float(scan['Ay'])], [float(scan['Az'])]]).__repr__()
 
 test_coordinates = ransac.ConvertToCartesianEulerAngles(scan_kalman)
+(scaled_coordinates, factor) = ransac.NormalizeCartesian(test_coordinates)
 
-xs = []
-ys = []
-zs = []
-for point in test_coordinates.values():
-    xs.append(point[0])
-    ys.append(point[1])
-    zs.append(point[2])
 
-(Landmarks_New, LSRP_list, Unassociated_Points) = ransac.RANSAC(test_coordinates, X, C, N, S, S_LIM)
+(Landmarks_New, LSRP_list, Unassociated_Points, Associated_Points) = ransac.RANSAC(scaled_coordinates, X, C, N, S, S_LIM)
+print("RANSAC found "+str(len(LSRP_list))+" LSRP's!")
 print(LSRP_list)
+
+## Assemble lists of the unassociated point coordinates for visualizations
+xun = []
+yun = []
+zun = []
+for point in Unassociated_Points.values():
+    xun.append(point[0])
+    yun.append(point[1])
+    zun.append(point[2])
+
+## Assemble lists of the associated point coordinates for visualizations
+xa = []
+ya = []
+za = []
+for point in Associated_Points.values():
+    xa.append(point[0])
+    ya.append(point[1])
+    za.append(point[2])
 
 print("Plotting Points and Landmarks...")
 fig = plt.figure()
 plt.clf()
 ax = Axes3D(fig)
-ax.scatter(xs, ys, zs, s=1, marker='o', color='r')
-#    ax.scatter(xfs, yfs, zfs, s=1, marker='x', color='b')
+ax.scatter(xun, yun, zun, s=1, marker='o', color='r')
+ax.scatter(xa, ya, za, s=1, marker='x', color='b')
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
-##ax.set_xlim3d(-500, 500)
-##ax.set_ylim3d(-1000, 3000)
-#    ax.set_zlim3d(-2000, 2000)
-ransac.plotLSRPs(ax, LSRP_list, ymax=500)
+ax.set_xlim3d(-1, 1)
+ax.set_ylim3d(-1, 1)
+ax.set_zlim3d(0, 1)
+ransac.plotLSRPs(ax, LSRP_list)
 ax.view_init(45, -90)
 plt.show()
